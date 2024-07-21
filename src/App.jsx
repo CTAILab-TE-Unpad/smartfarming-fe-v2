779db2cb78927e4ce6406661fba2b34e15abf997
lidiamarserl <141480899+lidiamarserl@users.noreply.github.com>
2024-07-21 08:49:54 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1030,7 +1030,7 @@ function App() {
 											<div className="flex flex-row gap-3 px-2 py-2 justify-between w-full font-medium border-b border-solid border-zinc-100">
 												<div className="flex flex-col gap-4 font-medium w-full text-center justify-center items-center">
 													{new Date(
-														dataB?.musan1?.createdAt
+														data?.sensorn1?.createdAt
 													).toLocaleDateString()}{" "}
 													<br />
 												</div>
